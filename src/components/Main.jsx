634d4e7111211102e0934865813a17797ba56ccf
NomanAhmed <officialnoman515@gmail.com>
2024-10-15 15:39:56 +0300

    working on main page

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,7 +4,22 @@ import mainImage from "../assets/furniture1.avif";
 const Main = () => {
   return (
     <>
-     
+      <div className="px-[20px] overflow-x-hidden pt-[20px] pb-[30px]">
+        <div className="flex flex-col my-[50px] gap-[30px] justify-around md:flex-row items-center">
+          <div className="flex flex-col gap-4 max-w-[500px]">
+            <h1 className="text-[30px] md:text-[50px] font-bold">
+              Home of quality furniture
+            </h1>
+            <p className="text-slate-700">
+              Welcome to HomeDecor , your one-stop shop for stylish furniture
+              and home decor
+            </p>
+          </div>
+          <div>
+            <img src={mainImage} alt="" />
+          </div>
+        </div>
+      </div>
     </>
   );
 };
